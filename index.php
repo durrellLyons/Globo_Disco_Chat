aad97d47c8fb0b049aea60294db507b6a1bb0a9d
Durrell Lyons <durrell.lyons@gmail.com>
2017-08-02 15:53:19 -0400

    purging unused files

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php include('inc/header.php'); ?>
-<div id='content'></div>
-<?php include('inc/footer.php'); ?>
-<script type='text/javascript'>
-    var loginScreen = 'inc/login';
-    var contentDiv = 'content';
-    document.getElementById('content').onload = indexObject.loadView(loginScreen, contentDiv);
-    indexObject.deleteCookie();
-</script>
